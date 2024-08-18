7e2fd386ace4fac2fb2c425939f2c58ab6deee81
gypark <a1061602@gmail.com>
2024-08-19 08:58:38 +0900

    feat: spot 노드 수정

--- node-pools.tf
+++ node-pools.tf
@@ -62,7 +62,7 @@ resource "google_container_node_pool" "spot" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "e2-small"
 
     labels = {
       team = "devops"
