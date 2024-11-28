66cfd6edd208ff75697a37f87424e5f9e78741ac
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2024-11-28 15:22:40 +0900

    feat: tt 파일을 gcp에 맞게 재설정

--- firewalls.tf
+++ firewalls.tf
@@ -1,11 +1,23 @@
 # Google Compute Firewall 설정
 resource "google_compute_firewall" "allow-ssh" {
-  name = var.firewall_name
+  name = var.ssh_firewall_name
   network = google_compute_network.main.name
 
   allow {
     protocol = "tcp"
     ports = ["22"]
+  }
+
+  source_ranges = var.firewall_source_ranges
+}
+
+resource "google_compute_firewall" "allow-http-https" {
+  name = var.http_https_firewall_name
+  network = google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "443"]
   }
 
   source_ranges = var.firewall_source_ranges
